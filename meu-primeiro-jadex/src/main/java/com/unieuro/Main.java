b157e66fed221a9c848fd34e4fe651327114f509
Rafael Marconi Ramos <88451906+rafaelcleversystems@users.noreply.github.com>
2025-09-18 13:04:10 +0000

    Primeiro commit com  jadex utilizando maven

---
+++ meu-primeiro-jadex/src/main/java/com/unieuro/Main.java
@@ -0,0 +1,11 @@
+package com.unieuro;
+
+import com.unieuro.agents.HelloAgent;
+
+public class Main {
+    public static void main(String[] args) {
+
+        HelloAgent.main(args);
+        //System.out.println("Hello world!");
+    }
+}
